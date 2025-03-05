8e718f87f7e3db98545046cdebf50b2b47b9b5d9
lamnk-sbt <lamnk@sabitech.vn>
2025-03-05 11:04:24 +0700

    update code

--- pages/about/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <h1 data-aos="fade-right" class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  <h1 data-aos="fade-up" class="text-3xl font-bold underline">Hello world!</h1>
-</template>
